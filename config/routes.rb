c890ec1a2ed46438aac20c9cde45089278a96d25
jayrcastro29 <castroarmandojr29@gmail.com>
2018-06-13 18:26:21 +0800

    error

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,14 @@ Rails.application.routes.draw do
   resources :user_details
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'user_details#index'
+ # devise_scope :user do
+ #  authenticated :user do
+    root 'welcome#index'#, as: :authenticated_root
+ #  end
+
+ #  unauthenticated do
+ #    root 'devise/sessions#new', as: :unauthenticated_root
+ #  		end
+	# end
+	# root 'devise/sessions#new'
 end
